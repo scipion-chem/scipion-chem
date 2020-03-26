540adc213335e615dff1f6b64e142b20575c705c
Carlos Oscar Sorzano <coss@cnb.csic.es>
2020-03-26 23:07:29 +0100

    Remove duplicates added

---
+++ atomstructutilsWeb/protocols/protocol_listIDs_operate.py
@@ -0,0 +1,97 @@
+# **************************************************************************
+# *
+# * Authors:     Carlos Oscar Sorzano (coss@cnb.csic.es)
+# *
+# * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+
+import copy
+import os
+import sys
+
+from pwem.protocols import EMProtocol
+from pyworkflow.protocol.params import PointerParam, EnumParam, MultiPointerParam, BooleanParam
+from atomstructutilsWeb.objects import DatabaseID, SetOfDatabaseID
+
+class ProtAtomStructListOperate(EMProtocol):
+    """This protocol will remove all duplicated entries using the DbID as key"""
+    _label = 'operate list'
+
+    def _defineParams(self, form):
+        form.addSection(label='Input')
+        form.addParam('operation', EnumParam, choices=['Unique', 'Union', 'Intersection', 'Difference'],
+                      label='Operation', default=0,
+                      help='Unique: Remove replicated Ids.')
+        form.addParam('inputListID', PointerParam, pointerClass="SetOfDatabaseID",
+                       label='List of DB Ids:', allowsNull=True, condition='(operation!=1)')
+        form.addParam('multipleInputListID', MultiPointerParam, pointerClass="SetOfDatabaseID",
+                       label='List of DB Ids:', allowsNull=True, condition='(operation==1)')
+        form.addParam('inputListID2', PointerParam, pointerClass="SetOfDatabaseID",
+                       label='List of DB Ids:', allowsNull=True, condition='(operation>=2)')
+        form.addParam('removeDuplicates', BooleanParam, default=False,
+                       label='Remove duplicates:', condition='(operation!=1)')
+
+    # --------------------------- INSERT steps functions --------------------
+    def _insertAllSteps(self):
+        self._insertFunctionStep('operateStep')
+
+    def operateStep(self):
+        ligandDict = {}
+        if self.operation.get()==1:
+            # Union
+            for database in self.multipleInputListID:
+                for databaseEntry in database.get():
+                    add=True
+                    if self.removeDuplicates.get():
+                        add=not databaseEntry.getDbId() in ligandDict
+                    if add:
+                        dbEntry = DatabaseID()
+                        dbEntry.copy(databaseEntry, copyId=False)
+                        ligandDict[databaseEntry.getDbId()]=dbEntry
+        else:
+            ligandList2 = []
+            if self.operation.get()==2 or self.operation.get()==3:
+                for databaseEntry in self.inputListID2.get():
+                    ligandList2.append(databaseEntry.getDbId())
+
+            for databaseEntry in self.inputListID.get():
+                add=False
+                if self.operation.get()==0:
+                    add = not databaseEntry.getDbId() in ligandDict
+                elif self.operation.get()==2:
+                    add = databaseEntry.getDbId() in ligandList2
+                    if self.removeDuplicates.get():
+                        add=add and not databaseEntry.getDbId() in ligandDict
+                elif self.operation.get()==3:
+                    add = not databaseEntry.getDbId() in ligandList2
+                    if self.removeDuplicates.get():
+                        add = add and not databaseEntry.getDbId() in ligandDict
+                if add:
+                    dbEntry = DatabaseID()
+                    dbEntry.copy(databaseEntry)
+                    ligandDict[databaseEntry.getDbId()]=dbEntry
+
+        outputDatabaseID = SetOfDatabaseID().create(path=self._getPath())
+        for dbId in ligandDict:
+            outputDatabaseID.append(ligandDict[dbId])
+        self._defineOutputs(output=outputDatabaseID)
+        self._defineSourceRelation(self.inputListID, outputDatabaseID)
