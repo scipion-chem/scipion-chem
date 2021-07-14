35ec888b855a9df0ec4c6b3bc38c572d321991dd
Alberto M. Parra-Pérez <59636474+AlbertoMPP@users.noreply.github.com>
2021-07-14 11:36:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='scipion-em-pwchem',
+    name='scipion-chem',
     version='3.0.0',
     description='A Scipion plugin for atomic models utilities in the web',
     long_description=long_description,
