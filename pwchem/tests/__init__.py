23d8802913fcda483c845d0792e6841cade30b29
DaniDelHoyo <danieldelhoyog@gmail.com>
2021-09-27 17:31:17 +0200

    Small molecules from dataset

--- pwchem/tests/__init__.py
+++ pwchem/tests/__init__.py
@@ -29,7 +29,3 @@ from .test_import_small_Molecules import TestImportSmallMolecules
 from .test_list_operate import TestListOperate
 from .test_export_csv import TestExportcsv
 
-DataSet(name='ligandLibraries',
-        folder='ligandLibraries',
-        files={}
-        )
