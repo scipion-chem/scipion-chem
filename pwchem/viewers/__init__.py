066ad9a258b09f865d0d4dc68d73f2656b15c4e3
DaniDelHoyo <danieldelhoyog@gmail.com>
2023-01-20 13:46:25 +0100

    Viewer MDSystem centralized in chem

--- pwchem/viewers/__init__.py
+++ pwchem/viewers/__init__.py
@@ -1,9 +1,7 @@
-from .viewer_protocol_dali import ProtChemDaliViewer
-from .viewer_protocol_ZL_predict import ProtChemZLPredictViewer
-from .viewer_protocol_raptorX import ProtChemRaptorXViewer
 from .viewers_data import *
 from .viewer_pockets import *
 from .viewer_smallMols import SmallMoleculesViewer
 from .viewers_sequences import SequenceAliViewer
 from .viewer_pharmacophore import *
 from .viewer_structure_attributes import ConservationViewer, SASAStructureViewer
+from .viewers_MD import *
