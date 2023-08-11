fd66a9799ece397e99824e60c30c716161988771
DaniDelHoyo <danieldelhoyog@gmail.com>
2023-08-11 19:25:29 +0200

    .

--- pwchem/protocols/VirtualDrugScreening/protocol_ligand_filter.py
+++ pwchem/protocols/VirtualDrugScreening/protocol_ligand_filter.py
@@ -1,10 +1,10 @@
 #Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
 # # -*- coding: utf-8 -*-
-# # # **************************************************************************
-# # # *
-# # # * Authors: Alba Lomas Redondo (albalomasredon@gmail.com)
-# # # *
-# # # *
+# **************************************************************************
+# *
+# * Authors: Daniel Del Hoyo (ddelhoyo@cnb.csic.es)
+# *
+# *
 # * This program is free software; you can redistribute it and/or modify
 # * it under the terms of the GNU General Public License as published by
 # * the Free Software Foundation; either version 2 of the License, or
