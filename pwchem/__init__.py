35f4d20ad8d2fe7578043f44758cd66edb4bb9ef
DaniDelHoyo <danieldelhoyog@gmail.com>
2025-02-12 11:52:40 +0100

    ODDT installation fixed

--- pwchem/__init__.py
+++ pwchem/__init__.py
@@ -115,7 +115,7 @@ class Plugin(pwem.Plugin):
 		# Installing package
 		rdkitEnvName = cls.getEnvName(RDKIT_DIC)
 		installer.addCommand(f'conda create -c conda-forge --name {rdkitEnvName} '
-												 f'{RDKIT_DIC["name"]}={RDKIT_DIC["version"]} python=3.10 -y', 'RDKIT_ENV_CREATED')\
+												 f'{RDKIT_DIC["name"]}={RDKIT_DIC["version"]} oddt=0.7 python=3.10 -y', 'RDKIT_ENV_CREATED')\
 			.addCommand('mkdir oddtModels', 'ODTMODELS_CREATED')\
 			.addPackage(env, dependencies=['conda'], default=default, vars={'PATH': env_path} if env_path else None)
 			
