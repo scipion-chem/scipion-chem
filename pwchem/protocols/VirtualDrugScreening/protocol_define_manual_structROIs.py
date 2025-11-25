c37dac26f7267cdc3a6d55f7eb3aab559e660e4f
Daniel Del Hoyo Gomez <danieldelhoyog@gmail.com>
2025-11-25 18:50:32 +0100

    pass removed

--- pwchem/protocols/VirtualDrugScreening/protocol_define_manual_structROIs.py
+++ pwchem/protocols/VirtualDrugScreening/protocol_define_manual_structROIs.py
@@ -383,7 +383,7 @@ class ProtDefineStructROIs(EMProtocol):
                     residue = self.structModel[chainId][resId]
                 except KeyError:
                     print(f'{resId} not in chain {chainId}')
-                    pass
+                    
                 atoms = residue.get_atoms()
                 for a in atoms:
                   oCoords.append(list(a.get_coord()))
