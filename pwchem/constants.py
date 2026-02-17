350b69fdb26bc7447d57a3177f25d854256d9b72
judith-MC <judith3atk@gmail.com>
2026-02-17 16:19:52 +0100

    Add ranx normalization method

--- pwchem/constants.py
+++ pwchem/constants.py
@@ -468,7 +468,7 @@ hide everything, not br. all within 3 of (byres polymer & name CA)
 set movie_fps, 15
 '''
 
-NORM_STRATEGY = ["None", "min-max", "min-max-inverted", "max", "sum", "rank", "borda"]
+NORM_STRATEGY = ["None", "min-max", "min-max-inverted", "max", "sum", "zmuv", "rank", "borda"]
 SCORE_BASED_METHODS = ["med", "anz"]
 RANK_BASED_METHODS = ["isr", "log_isr", "logn_isr", "rrf", "rbc"]
 
