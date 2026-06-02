91a0dd9750bcc040c9a199800eb3a7f83b37413e
Blanca Pueche <blancapueche@gmail.com>
2026-06-02 10:36:13 +0200

    sequence filename

--- pwchem/protocols/Sequences/protocol_import_setOfSequences.py
+++ pwchem/protocols/Sequences/protocol_import_setOfSequences.py
@@ -127,11 +127,10 @@ class ProtChemImportSetOfSequences(EMProtocol):
 
         isAmino = self.database.get() in [1]
         for outFn in outFns:
-            newSeq = Sequence()
+            newSeq = Sequence(fileName=outFn)
             newSeq.importFromFile(outFn, isAmino=isAmino)
             seqName = os.path.splitext(os.path.basename(outFn))[0]
             newSeq.setSeqName(seqName)
-            newSeq.setFileName(outFn)
             outputSequences.append(newSeq)
 
         self._defineOutputs(outputSequences=outputSequences)
