bc12bb1d9c4abdfad4127a48e2ae627691d402f7
Laura Pérez Liens <lapeli1998@gmail.com>
2026-09-25 11:37:29 +0200

    fix wizard error

--- pwchem/wizards/wizard_genomes.py
+++ pwchem/wizards/wizard_genomes.py
@@ -32,7 +32,7 @@ from pwem.wizards import VariableWizard
 from pwchem.protocols.Sequences.protocol_reference_genomes import ProtReferenceGenomes
 from pwchem.protocols.Sequences.protocol_vcf import ProtVCF
 from pwchem.protocols.Sequences.protocol_rnaseq_align import ProtRNASeqAlignment
-from pwchem.utils.utilsFasta import getCommonSpecies
+from pwchem.utils.utilsRNA import getCommonSpecies
 
 
 class SelectCommonSpeciesWizard(VariableWizard):
